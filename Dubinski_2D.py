b6c439a4e24f57b0cfc950ca377f8ada923b94fe
Thomas Seive <thomasseive@Geoffroys-MacBook-Air.local>
2021-01-21 19:19:50 -0600

    A complete functioning version of the 2D code

--- Dubinski_2D.py
+++ Dubinski_2D.py
@@ -6,6 +6,7 @@ def axis(arr_in, rad, shell=False, axes_out=False, fix_volume=True, quiet=False)
     WORK IN PROGRESS -- Needs to be checked.
     2D VERSION -- Worked on by Thomas Seive. Primary purpose is to calculate
     the ellipticity of a galaxy while only considering one projection (two axes).
+    This version cannot make use of the shell feature found in the 3d version
     Here is just a small change to the code to try and test this github push/pull feature
     Here is another line that I've added. Slowly getting the hang of this git stuff!
     Adding this line for the github test on Mac; another test to see if I can add without
@@ -13,6 +14,7 @@ def axis(arr_in, rad, shell=False, axes_out=False, fix_volume=True, quiet=False)
     Managed to make a change on my mac and get the change on my pc! Let's do the reverse
     Don't even need to close the file for it to refresh with the new changes! (at least on mac)
     And the same on PC! I can now officially use github to work on my code :-)
+    End 2D code//
 
     May want to add capability to compute axis ratios at multiple
     different radii in one function call (w/ a loop?) 
@@ -45,7 +47,8 @@ def axis(arr_in, rad, shell=False, axes_out=False, fix_volume=True, quiet=False)
         tensor=np.zeros([2,2])
         # given initial values for primary axes, compute ellipsoidal
         # radius of each particle (rp2 is not square rooted because it is squared in the next step, i.e. they cancel)
-        rp2=(arr_in[:,0]**2/axrat_in[0]**2 + arr_in[:,1]**2/axrat_in[1]**2) #(Thomas)changed this line
+        rp2=(arr_in[:,0]**2/axrat_in[0]**2 + arr_in[:,1]**2/axrat_in[0]**2) #(Thomas)changed this line
+        #made axrat_in call only the [0] element because I will only have one axial ratio to guess
 
         # construct the moment of inertial tensor to be diagonalized:
         for i in range(2): #(Thomas)changed this line, rp2 is the "a" from the paper
@@ -61,8 +64,9 @@ def axis(arr_in, rad, shell=False, axes_out=False, fix_volume=True, quiet=False)
     cnt=0
     # initial guess for principal axes:
     evs0=np.array([[1e0,0e0],[0e0, 1e0]]) #(Thomas) changed this line
-    # initial guess for axis ratios:
-    axes=np.array([1e0,1e0])
+    # initial guess for axis ratios: 
+    axes=np.array([1e0]) #(Thomas) changed this line: only give one initial guess because we are only calculating one 
+    #axial ratio
     avdiff=1e0
 
     rad=np.asarray(rad)
@@ -73,13 +77,15 @@ def axis(arr_in, rad, shell=False, axes_out=False, fix_volume=True, quiet=False)
         # compute ellipsoidal radius of each particle:  (Thomas)This is a radius for each particle
         dist2=(
                arr_in[:,0]**2/axes[0]**2 +
-               arr_in[:,1]**2/axes[1]**2)**0.5
+               arr_in[:,1]**2/axes[0]**2)**0.5
             #(Thomas)Changed the line above
         # find locations of particles between rad[0] and rad[1]
         if not fix_volume:
             r_ell=rad
         else:
-            r_ell=(rad**3/axes[0]/axes[1])**(1e0/3e0)
+            r_ell=(rad**3/axes[0]/axes[0])**(1e0/3e0) #(Thomas): Broke this line so
+            #dont use the shells! 
+            
  
         # current version keeps volume same as original spherical
         # volume rather than keeping principal axis same as
@@ -95,7 +101,7 @@ def axis(arr_in, rad, shell=False, axes_out=False, fix_volume=True, quiet=False)
         # get eigenvectors and eigenvalues.  Note: the inertia tensor
         # is a real symmetric matrix, so the eigenvalues should be real.
         # locs is the location (radius) for particles
-        # axtemp is the current axial ratios (q/s) 
+        # axtemp is the current axial ratios (q & s) 
         axrat=calc_inertia(arr_in[locs], axtemp)
         if abs(np.imag(axrat[0])).max() > 0.:
             raise ValueError('Error: eigenvalues are not all real!')
@@ -104,7 +110,7 @@ def axis(arr_in, rad, shell=False, axes_out=False, fix_volume=True, quiet=False)
         evecs=axrat[1]
         # get axis ratios from eigenvalues: (Thomas) Changed this line; have no third component with which
         # to calculate q and s
-        axes=np.sqrt([evals[1]/evals[0], 
+        axes=np.sqrt([evals[1]/evals[0] 
                          ])
         # rotate particles (and previous eigenvector array) into new
         # basis:
@@ -122,7 +128,7 @@ def axis(arr_in, rad, shell=False, axes_out=False, fix_volume=True, quiet=False)
             # print axis ratios relative to major axis
             print ('axis ratios: ', (np.sort(evals/evals.max()))**0.5)
             print ('deviations from previous iteration: ' + \
-            '%.*e, %.*e, %.*e' % (4, avd0, 4, avd1, 4, avd2))
+            '%.*e, %.*e' % (4, avd0, 4, avd2))
             print ('number of particles in shell / sphere: ', np.size(locs))
         avdiff=max(avd0, avd2)
         cnt+=1
@@ -137,7 +143,7 @@ def axis(arr_in, rad, shell=False, axes_out=False, fix_volume=True, quiet=False)
     'and corresponding unit eigenvectors are:')
     print ('%.*f' % (5, evals[inds[0]]), evs0[:,inds[0]])
     print ('%.*f' % (5, evals[inds[1]]), evs0[:,inds[1]])
-    print ('%.*f' % (5, evals[inds[2]]), evs0[:,inds[2]])
+    #print ('%.*f' % (5, evals[inds[2]]), evs0[:,inds[2]])
     print ('axis ratios: ', final_axrats)
     if axes_out:
         return final_axrats, [evs0[:,inds[0]], evs0[:,inds[1]], evs0[:,inds[2]]]
